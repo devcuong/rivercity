e7145127237550fdfd7dd4c547cb9407f8b81f2c
cuong1503 <cuong.dh8c@gmail.com>
2016-07-24 17:14:12 +0700

    mn

---
+++ src/main/resources/META-INF/sql/News_Insert_01.sql
@@ -0,0 +1,6 @@
+INSERT INTO news_real (news_title, news_description, news_image, news_alias, news_content)
+VALUES (:newsTitle,
+        :newsDescription,
+        :newsImage,
+        :newsAlias,
+        :newsContent)
