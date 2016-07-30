88176a9f41bd4b6599a13bddcfd9c4e0c9478836
cuong1503 <cuong.dh8c@gmail.com>
2016-07-30 08:50:01 +0700

    sell post

---
+++ src/main/java/com/server/career/constant/BussinessConstant.java
@@ -0,0 +1,73 @@
+package com.server.career.constant;
+
+public class BussinessConstant {
+
+	/**
+	 * Các tỉnh thành
+	 */
+	public static final String HCM = "Thành Phố Hồ Chí Minh";
+	public static final String HN = "Hà Nội";
+	public static final String BD = "Bình Dương";
+	public static final String DN = "Đà Nẵng";
+	public static final String HP = "Hải Phòng";
+	public static final String LA = "Long An";
+	public static final String BRVT = "Bà Rịa Vũng Tàu";
+	public static final String AG = "An Giang";
+	public static final String BG = "Bắc Giang";
+	public static final String BK = "Bắc Kạn";
+	public static final String BL = "Bạc Liêu";
+	public static final String BN = "Bắc Ninh";
+	public static final String BTR = "Bến Tre";
+	public static final String BDD = "Bình Định";
+	public static final String BP = "Bình Phước";
+	public static final String BTH = "Bình Thuận";
+	public static final String CM = "Cà Mau";
+	public static final String CT = "Cần Thơ";
+	public static final String CB = "Cao Bằng";
+	public static final String DDL = "Đắk Lắk";
+	public static final String DNO = "Đắk Nông";
+	public static final String DDB = "Điện Biên";
+	public static final String DNA = "Đồng Nai";
+	public static final String DDT = "Đồng Tháp";
+	public static final String GL = "Gia Lai";
+	public static final String HG = "Hà Giang";
+	public static final String HNA = "Hà Nam";
+	public static final String HT = "Hà Tĩnh";
+	public static final String HD = "Hải Dương";
+	public static final String HGI = "Hậu Giang";
+	public static final String HB = "Hòa Bình";
+	public static final String HY = "Hưng Yên";
+	public static final String KH = "Khánh Hòa";
+	public static final String KG = "Kiên Giang";
+	public static final String KT = "Kon Tum";
+
+	/**
+	 * Mã loại của hình thức bán
+	 */
+	public static final String BCHCC = "Bán căn hộ chung cư";
+	public static final String BNR = "Bán nhà riêng";
+	public static final String BNBTLK = "Bán nhà biệt thự liền kề";
+	public static final String BNMP = "Bán nhà mặt phố";
+	public static final String BDNDA = "Bán đất nền dự án";
+
+	/**
+	 * Mã loai của hình thức mua
+	 */
+	public static final String TCHCC = "Thuê căn hộ chung cư";
+	public static final String TNR = "Thuê nhà riêng";
+	public static final String TNMP = "Thuê nhà mặt phố";
+	public static final String TNTPT = "Thuê nhà trọ phòng trọ";
+	public static final String TVP = "Thuê văn phòng";
+	public static final String TCHKO = "Thuê cửa hàng, kiot";
+	public static final String TKNXD = "Thuê kho, nhà xưởng, đất";
+	public static final String TBDSK = "Thuê bất động sản khác";
+
+	/**
+	 * Đơn vị giá hình thức bán
+	 */
+	public static final String BGTT = "Thỏa thuận";
+	public static final String BGTR = "Triệu";
+	public static final String BGT = "Tỷ";
+	public static final String BGTNM = "Trăm nghìn/m2";
+	public static final String BGTRM = "Triệu/m2";
+}
